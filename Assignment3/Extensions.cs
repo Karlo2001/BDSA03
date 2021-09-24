fb0bc1c3f5d35cac7d0da293acec82b7a509947c
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-24 13:50:21 +0200

    One liners

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace BDSA2020.Assignment02
+namespace BDSA2020.Assignment03
 {
     public static class Extensions
     {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> input) => input.SelectMany(x => x).ToList();
+        public static int[] DivisibleAndGreater(this int[] input) => input.Where(x => x > 42 && x % 7 == 0).ToArray();
+        public static int[] LeapYears(this int[] input) => input.Where(x => x % 4 == 0 && (x % 100 != 0 || x % 400 == 0)).ToArray();
     }
 }
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using BDSA2020.Assignment03;
 
 namespace Assignment3
 {
@@ -6,7 +9,12 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<IEnumerable<int>> xs = new List<List<int>> {new List<int>{1,2,3,4}, new List<int>{1,2,3,4}};
+            IEnumerable<int> result = xs.Flatten();
+            int[] ys = new int[] {49,1,2,3,4,5,6};
+            int[] res = ys.DivisibleAndGreater();
+            int[] ys2 = new int[]{2000, 2004, 2008, 100, 200, 1700, 1600};
+            int[] res2 = ys2.LeapYears();
         }
     }
 }
