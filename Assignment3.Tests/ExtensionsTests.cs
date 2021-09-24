be1b5dc8d243d2681993420333bf7044335cea0f
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-24 14:18:23 +0200

    Fix test and namespaces

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -6,16 +6,25 @@ namespace BDSA2020.Assignment02.Tests
 {
     public class ExtensionsTests
     {
-        var test1 = new Uri("https://test.com");
-        var test2 = new Uri("www.test.com");
-        [Theory]
-        [InlineData(test1, true)]
-        [InlineData(test2, false)]
-        public void IsSecure_Returns_True_If_HTTPS(Uri input, bool expected)
+        
+        
+        [Fact]
+        public void IsSecure_Returns_True_For_HTTPS()
         {
+            var input = new Uri("https://test.com");
+
             var res = input.IsSecure();
 
-            Assert.Equal(expected, res);
+            Assert.True(res);
+        }
+        [Fact]
+        public void IsSecure_Returns_False_For_Non_HTTPS()
+        {
+            var input = new Uri("http://www.test.com");
+
+            var res = input.IsSecure();
+
+            Assert.False(res);
         }
     }
 }
