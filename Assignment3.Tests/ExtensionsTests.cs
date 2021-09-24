9689bbb6e3772ad9e316113bb7c165cd84fe5b3a
Karl Michelsen <43517053+Karlo2001@users.noreply.github.com>
2021-09-24 14:04:21 +0200

    Share extension code

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
+using BDSA2020.Assignment03;
 
 namespace BDSA2020.Assignment02.Tests
 {
     public class ExtensionsTests
     {
-        [Fact]
-        public void Test1()
+        var test1 = new Uri("https://test.com");
+        var test2 = new Uri("www.test.com");
+        [Theory]
+        [InlineData(test1, true)]
+        [InlineData(test2, false)]
+        public void IsSecure_Returns_True_If_HTTPS(Uri input, bool expected)
         {
+            var res = input.IsSecure();
 
+            Assert.Equal(expected, res);
         }
     }
 }
--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -9,5 +9,6 @@ namespace BDSA2020.Assignment03
         public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> input) => input.SelectMany(x => x).ToList();
         public static int[] DivisibleAndGreater(this int[] input) => input.Where(x => x > 42 && x % 7 == 0).ToArray();
         public static int[] LeapYears(this int[] input) => input.Where(x => x % 4 == 0 && (x % 100 != 0 || x % 400 == 0)).ToArray();
+        public static bool IsSecure(this Uri input) => input.Scheme == Uri.UriSchemeHttps;
     }
 }
